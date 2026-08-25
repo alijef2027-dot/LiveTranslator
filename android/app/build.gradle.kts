236a20835e6448bc2b534203bbcf821c6d3a4668
alijef2027-dot <ali.jef.2027@gmail.com>
2026-08-25 12:07:25 +0300

    Fix SDK and NDK versions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,45 +1,40 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    // Flutter Gradle Plugin
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.voicebridge.app"
-    compileSdk = 34
-    ndkVersion = flutter.ndkVersion
+    namespace = "com.example.voicebridge"
+    compileSdk = 35
+    ndkVersion = "25.1.8937393"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "1.8"
     }
 
     defaultConfig {
-        applicationId = "com.voicebridge.app"
-        minSdk = 23
-        targetSdk = 34
+        applicationId = "com.example.voicebridge"
+        minSdk = 24
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
     }
 
     buildTypes {
         release {
+            // Signing with the debug keys for now
             signingConfig = signingConfigs.getByName("debug")
-            isMinifyEnabled = false
-            isShrinkResources = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 }
 
 flutter {
     source = "../.."
-}
-
-dependencies {
-    implementation("androidx.window:window:1.2.0")
 }
